d33c5bfad920ab675854ae75582fa4b02cce72a7
elrostov <qepgiq-sovcan-6roqWy>
2020-01-27 17:51:47 +0300

    pre8 with EntityManager, no HibernateConfig

--- src/main/java/org/example/security/SecurityConfig.java
+++ src/main/java/org/example/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.example.config;
+package org.example.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
